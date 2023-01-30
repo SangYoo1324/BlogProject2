444ee17182698d744561d18c6840ec9182050899
sam <samuel13241@gmail.com>
2023-01-29 21:54:48 -0800

    no message

--- src/main/java/blog/project/repository/BoardRepository.java
+++ src/main/java/blog/project/repository/BoardRepository.java
@@ -8,8 +8,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface  BoardRepository  extends JpaRepository<Board, Long> {
+
+    // mysql 쿼리
+//    @Modifying(clearAutomatically = true,flushAutomatically = true)
+//    @Query(value ="UPDATE Board b SET b.count= b.count+1 WHERE b.board_id =:board_id",nativeQuery = true)
+//    void updateView(Long board_id);
+
+    //postgres 쿼리
     @Modifying(clearAutomatically = true,flushAutomatically = true)
-    @Query(value ="UPDATE Board b SET b.count= b.count+1 WHERE b.board_id =:board_id",nativeQuery = true)
+    @Query(value ="UPDATE Board  SET count = count+1 WHERE board_id =1;",nativeQuery = true)
     void updateView(Long board_id);
-    }
+}
+
+
 
